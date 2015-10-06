01d218b255f267bbb8b0fb2cc1d309f7f1d70aef
fmaredia <farhana.maredia@gmail.com>
2015-10-06 10:12:11 -0500

    make it better

--- server/index.js
+++ server/index.js
@@ -2,6 +2,21 @@ var browserify = require('browserify-middleware')
 var express = require('express')
 var app = express()
 var Path = require('path')
+var MongoClient = require('mongodb').MongoClient
+var assert = require('assert')
+var url = 'mongodb://localhost:27017/trebekardy'
+// var url = process.env.MONGOLAB_URI || config.mongodb
+var DB, questions;
+
+MongoClient.connect(url, function(err, db) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Correctly connects to the database');
+    DB = db;
+    questions = DB.collection('questions');
+  }
+})
 
 // Provide a browserified file at a specified path
 app.get('/js/app-bundle.js',
@@ -11,6 +26,16 @@ app.get('/js/app-bundle.js',
 var assetFolder = Path.resolve(__dirname, '../client/public')
 app.use(express.static(assetFolder))
 
+app.get('/question', function(req, res) {
+
+	// var count = questions.count()
+	// var rand = function(){return Math.floor( Math.random() * count )}
+
+  var data = questions.find()
+
+  console.log(count)
+  res.send(questions.find())
+});
 
 //
 // Support browser history pushstate.
